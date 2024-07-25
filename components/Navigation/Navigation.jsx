a91816fa420e7ae73db8199f8745a8f3b66d6d28
mesganaG <geletumesgana@gmail.com>
2024-07-24 17:33:04 -0700

    Added initial structure for navigation bar

---
+++ components/Navigation/Navigation.jsx
@@ -0,0 +1,44 @@
+import "../../src/index.css";
+import "./Navigation.css";
+
+
+function Navigation() {
+    <>
+    <div className="main-container">
+
+        
+
+        <div className="left-side-container">
+
+     
+
+        <div className="dropdown-btn-container">
+
+        </div>
+
+        <div className="logo-container">
+
+        </div>
+
+        <div className="document-title-container">
+
+        </div>
+
+        </div>
+
+
+        <div className="right-side-container">
+
+        <div className="save-delete-container">
+
+        </div>
+
+        </div>
+       
+    </div>
+    
+    
+    
+    </>
+}
+export default Navigation;
